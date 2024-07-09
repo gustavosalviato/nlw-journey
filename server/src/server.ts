e7dd386cf6ab122f828fa955fdac991eaca2c0e7
gustavosalviato <gustavosalviato8@gmail.com>
2024-07-09 17:04:44 -0300

    feat: add create trip route

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log("Hello world");
+import fastify from "fastify";
+
+import {
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { createTrip } from "./routes/create-trip";
+
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createTrip);
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => console.log("http server running!"));
